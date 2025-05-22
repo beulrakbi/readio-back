2fb72f09b4e5db70afcb379506f014c131bf64e5
sohye <thgp1254@gmail.com>
2025-05-22 10:39:57 +0900

    깃 청소

--- src/main/java/com/team/teamreadioserver/interest/entity/UserInterestKeyword.java
+++ src/main/java/com/team/teamreadioserver/interest/entity/UserInterestKeyword.java
@@ -11,28 +11,28 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 
-//@Entity
-//@Table(name="user_interest_keyword")
-//public class UserInterestKeyword {
+@Entity
+@Table(name="user_interest_keyword")
+public class UserInterestKeyword {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "user_interest_keyword_id", updatable = false, nullable = false)
-//    private Long userInterestKeywordId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_interest_keyword_id", updatable = false, nullable = false)
+    private Long userInterestKeywordId;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
-//    private User userId;;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "interest_keyword_id", referencedColumnName = "interest_keyword_id")
-//    @Column(name = "interest_keyword_id", nullable = false)
-//    private Long interestId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User userId;;
 
-//    @Column(name = "created_at", nullable = false)
-//    private LocalDateTime createdAt = LocalDateTime.now();
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(name = "status", nullable = false)
-//    private InterestStatus status;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "interest_keyword_id", referencedColumnName = "interest_keyword_id")
+    @Column(name = "interest_keyword_id", nullable = false)
+    private Long interestId;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private InterestStatus status;
 }
