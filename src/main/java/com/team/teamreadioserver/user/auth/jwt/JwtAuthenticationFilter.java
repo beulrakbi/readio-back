e335add8e07aea88de420110b72ca891093f66af
BBB924 <qhrud4231@naver.com>
2025-05-23 17:41:09 +0900

    Feat: 로그인 기능 구현중(JWT방식) #20

--- src/main/java/com/team/teamreadioserver/user/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/team/teamreadioserver/user/auth/jwt/JwtAuthenticationFilter.java
@@ -1,40 +1,40 @@
-package com.team.teamreadioserver.user.auth.jwt;
-
-import com.team.teamreadioserver.user.auth.service.DetailsService;
-import jakarta.servlet.ServletException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
-    private final JwtTokenProvider jwtTokenProvider;
-    private final DetailsService userDetailsService;
-
-    public JwtAuthenticationFilter (JwtTokenProvider jwtTokenProvider, DetailsService userDetailsService) {
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.userDetailsService = userDetailsService;
-    }
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
-
-        String header = request.getHeader("Authorization");
-
-        if (header != null && header.startsWith("Bearer ")) {
-            String token = header.substring(7);
-            if (jwtTokenProvider.validateToken(token)) {
-                String userId = jwtTokenProvider.getUserIdFromToken(token);
-                UserDetails userDetails = userDetailsService.loadUserByUsername(userId);
-                UsernamePasswordAuthenticationToken authentication =
-                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
-        }
-        filterChain.doFilter(request, response);
-    }
-}
+//package com.team.teamreadioserver.user.auth.jwt;
+//
+//import com.team.teamreadioserver.user.auth.service.DetailsService;
+//import jakarta.servlet.ServletException;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import java.io.IOException;
+//
+//public class JwtAuthenticationFilter extends OncePerRequestFilter {
+//
+//    private final JwtTokenProvider jwtTokenProvider;
+//    private final DetailsService userDetailsService;
+//
+//    public JwtAuthenticationFilter (JwtTokenProvider jwtTokenProvider, DetailsService userDetailsService) {
+//        this.jwtTokenProvider = jwtTokenProvider;
+//        this.userDetailsService = userDetailsService;
+//    }
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+//                                    FilterChain filterChain) throws ServletException, IOException {
+//
+//        String header = request.getHeader("Authorization");
+//
+//        if (header != null && header.startsWith("Bearer ")) {
+//            String token = header.substring(7);
+//            if (jwtTokenProvider.validateToken(token)) {
+//                String userId = jwtTokenProvider.getUserIdFromToken(token);
+//                UserDetails userDetails = userDetailsService.loadUserByUsername(userId);
+//                UsernamePasswordAuthenticationToken authentication =
+//                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+//                SecurityContextHolder.getContext().setAuthentication(authentication);
+//            }
+//        }
+//        filterChain.doFilter(request, response);
+//    }
+//}
