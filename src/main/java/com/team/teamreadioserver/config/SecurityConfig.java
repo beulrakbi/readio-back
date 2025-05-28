0f609a726e73ccc9b2084f965665f7b69d122f91
sohye <thgp1254@gmail.com>
2025-05-28 12:25:44 +0900

    #38 로그인 후 관심사 CRUD

--- src/main/java/com/team/teamreadioserver/config/SecurityConfig.java
+++ src/main/java/com/team/teamreadioserver/config/SecurityConfig.java
@@ -52,6 +52,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/users/login", "/users/join/**").permitAll()  // 인증 필요없는 경로
                         .requestMatchers("/img/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/user/interests/categories", "/api/user/interests/keywords").permitAll()
+                        .requestMatchers("/api/user/**").authenticated()
                         .requestMatchers(
                                 "/",
                                 "/swagger-ui/**",
