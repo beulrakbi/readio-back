b386b551eff4d9b2a0d74ce854f67fc5c7e1f32a
BBB924 <qhrud4231@naver.com>
2025-05-29 10:41:48 +0900

    SecurityConfig: 주석 수정

--- src/main/java/com/team/teamreadioserver/config/SecurityConfig.java
+++ src/main/java/com/team/teamreadioserver/config/SecurityConfig.java
@@ -72,8 +72,8 @@ public class SecurityConfig {
                                 "/swagger-resources/**",
                                 "/webjars/**"
                         ).permitAll()
-                        .requestMatchers("/admin/**").permitAll()// 관리자 관련 경로
-//                        .requestMatchers("/admin/**").hasRole("ADMIN") // 관리자 관련 경로
+                        .requestMatchers("/admin/**").permitAll()       // 관리자 관련 경로(권한 풀고 테스트하면 403뜨는게 정상임 )
+//                       .requestMatchers("/admin/**").hasRole("ADMIN")   // 관리자 관련 경로(주석 해제시 해당경로는 관리자로 로그인해야 보임)
                         .anyRequest().authenticated()   // 그 외는 모두 로그인 필요
                 )
                 // JwtSecurityConfig 부분이랑 동일한 역할_JwtAuthenticationFilter를 SecurityFilterChain 안에서 등록
