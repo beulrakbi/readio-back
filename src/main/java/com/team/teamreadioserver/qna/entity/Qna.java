51093f9ada7ab0b6308fb00374f29dd4961f677c
hwangjaeyong <beomgolaeda@gmail.com>
2025-05-30 12:53:10 +0900

    갓보경

--- src/main/java/com/team/teamreadioserver/qna/entity/Qna.java
+++ src/main/java/com/team/teamreadioserver/qna/entity/Qna.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -35,10 +37,20 @@ public class Qna {
 
     @PrePersist
     public void prePersist() {
-        this.userId = "test2";
-        this.qnaCreateAt = LocalDateTime.now();
-        this.qnaView = 0;
+        if (this.qnaCreateAt == null) { // 혹시라도 이미 설정된 값이 없으면
+            this.qnaCreateAt = LocalDateTime.now();
+        }
+        if (this.userId == null) { // 혹시라도 이미 설정된 값이 없으면
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication != null && authentication.isAuthenticated()) {
+                this.userId = authentication.getName(); // 로그인한 아이디가 들어감
+            } else {
+                throw new IllegalStateException("인증된 사용자 정보를 찾을 수 없습니다. FAQ 작성은 로그인 후 가능합니다.");
+            }
+        }
     }
+
+
     public void updateQuestion(String title, String question) {
         this.qnaTitle = title;
         this.qnaQuestion = question;
