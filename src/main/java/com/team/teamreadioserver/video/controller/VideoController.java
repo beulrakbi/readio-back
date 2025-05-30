b849b95d183cbf27fe003d549254bafded8afd26
lurufarm <hsg200528@gmail.com>
2025-05-30 11:43:02 +0900

    #48 책 상세페이지 영상

--- src/main/java/com/team/teamreadioserver/video/controller/VideoController.java
+++ src/main/java/com/team/teamreadioserver/video/controller/VideoController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/video")
-@CrossOrigin(origins = "http://localhost:5173")
+//@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class VideoController {
 
