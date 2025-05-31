26650b9a96c36ea10925a77e6909ddf93770ca4a
hwangjaeyong <beomgolaeda@gmail.com>
2025-05-31 15:11:41 +0900

    북마크 페이지

--- src/main/java/com/team/teamreadioserver/bookmark/dto/VideoBookmarkResponseDTO.java
+++ src/main/java/com/team/teamreadioserver/bookmark/dto/VideoBookmarkResponseDTO.java
@@ -1,15 +1,16 @@
 package com.team.teamreadioserver.bookmark.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class VideoBookmarkResponseDTO {
+    private Integer bookmarkId;
     private String videoId;
-    private String userId;
+    private String videoTitle;    // 추가
+    private String channelTitle;  // 추가
+    private String thumbnailUrl;  // 추가 (Video 엔티티의 thumbnail 필드를 직접 넘겨줄 경우)
 }
