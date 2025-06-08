3675d9c6d8144301e254535c0c7c7e5856a9d5ac
soomin0927 <ehfvls0927@naver.com>
2025-06-08 12:44:01 +0900

    0608

--- src/main/java/com/team/teamreadioserver/config/RestTemplateConfig.java
+++ src/main/java/com/team/teamreadioserver/config/RestTemplateConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestTemplateConfig { // 수민 사용 !
+public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
